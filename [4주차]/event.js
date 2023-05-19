42d1acd42de1dc0cb873fe90e0e39a39b62c0559
benniejung <122111459+benniejung@users.noreply.github.com>
2023-05-19 18:26:25 +0900

    .
    .

--- [4주차]/event.js
+++ [4주차]/event.js
@@ -80,3 +80,15 @@ $("#B").click(function() {
     next();
 });
 
+function copy() {
+    var url = '';
+    var textarea = document.createElement("textarea");
+    document.body.appendChild(textarea);
+    url = window.document.location.href;
+    textarea.value = url;
+    textarea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textarea);
+    alert("링크가 복사되었습니다.");
+    
+}
