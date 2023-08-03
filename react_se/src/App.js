6affaa820ae38bb525f2f4cf825e3fdd5a14b337
Sengoo <lesley023@naver.com>
2023-08-03 17:07:05 +0900

    23-08-03 리액트 이세은

--- react_se/src/App.js
+++ react_se/src/App.js
@@ -4,14 +4,14 @@ import { Route, Router } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage/LandingPage"
 import LoginPage from "./pages/LoginPage/Login"
 import RegisterPage from "./pages/RegisterPage/RegisterPage"
-
+import { Routes } from 'react-router-dom';
 function App() {
   return (
-    <Router>
+    <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/Register" element={<RegisterPage />} />
-    </Router>
+    </Routes>
   );
 }
 
