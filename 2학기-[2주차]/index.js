488f942226e5e3b1a5cfbbaba9aa6b52b0152e52
drimh <107299318+drimh@users.noreply.github.com>
2023-08-10 16:07:55 +0900

    [23-08-10] 2주차 신승헤

---
+++ 2학기-[2주차]/index.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+// 하위 컴포넌트 정의
+class SubComponent extends Component {
+  render() {
+    return <div>{this.props.value}</div>;
+  }
+}
+
+// 상위 컴포넌트 정의
+class ParentComponent extends Component {
+  render() {
+    const valueToSend = "안녕하세요, 하위 컴포넌트에 전달하는 값입니다.";
+    
+    return (
+      <div>
+        <h1>상위 컴포넌트</h1>
+        <SubComponent value={valueToSend} />
+      </div>
+    );
+  }
+}
+
+export default ParentComponent;
