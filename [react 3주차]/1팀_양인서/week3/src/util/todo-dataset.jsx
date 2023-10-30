c879c7a0a96584bd71d734928ce4acaeedba966e
sheepyis <zx9086@naver.com>
2023-10-30 10:46:13 +0900

    create react 3주차 양인서

---
+++ [react 3주차]/1팀_양인서/week3/src/util/todo-dataset.jsx
@@ -0,0 +1,34 @@
+const TodoDataset = [
+    {
+        "id": 1,
+        "title": "웹프웤 프로젝트",
+        "isDone": false,
+        "img": "images/1.png"
+    },
+    {
+        "id": 2,
+        "title": "고모프 프로젝트",
+        "isDone": false,
+        "img": "images/2.png"
+    },
+    {
+        "id": 3,
+        "title": "네프 프로젝트",
+        "isDone": false,
+        "img": "images/3.png"
+    },
+    {
+        "id": 4,
+        "title": "4호선톤 프로젝트",
+        "isDone": false,
+        "img": "images/4.png"
+    },
+    {
+        "id": 5,
+        "title": "시험 공부",
+        "isDone": false,
+        "img": "images/5.png"
+    },
+]
+
+export default TodoDataset;
